259ecba1536117611b17974989b9fd73eab9f454
yongj.zhuang <yongj.zhuang@outlook.com>
2024-02-28 23:51:42 +0800

    Parse and compare version

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/curtisnewbie/svc
+
+go 1.20
+
+require github.com/spf13/cast v1.6.0
---
+++ version_test.go
@@ -0,0 +1,31 @@
+package svc
+
+import "testing"
+
+func TestPadVer(t *testing.T) {
+	v := []string{"1"}
+	v = PadVer(v, 3)
+	if len(v) != 3 {
+		t.Fatal("should be 3")
+	}
+	for i, s := range v {
+		if i == 0 && s != "1" {
+			t.Fatal("should be 1")
+		}
+		if i > 0 && s != "0" {
+			t.Fatal("should be 0")
+		}
+	}
+}
+
+func TestVerAfter(t *testing.T) {
+	if VerAfter("v1.1.3.4", "v2.0.3") {
+		t.Fatal("should return false")
+	}
+	if !VerAfter("v2", "v1.2.3") {
+		t.Fatal("should return true")
+	}
+	if !VerAfter("2.1", "1") {
+		t.Fatal("should return true")
+	}
+}
